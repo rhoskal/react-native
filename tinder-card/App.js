2caa393f5a05cd527ac86c5838ebe297238b9cb6
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-08-06 23:26:43 -0400

    initial
    initial

---
+++ tinder-card/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
