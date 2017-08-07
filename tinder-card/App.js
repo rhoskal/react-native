d0b9112b7e18cfeb050ac2a2964f2fda9be484e6
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-08-07 12:35:13 -0400

    working card
    working card

--- tinder-card/App.js
+++ tinder-card/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Animated, Image, cardPanResponder, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
+  componentWillMount() { //I think he's trying to create a constructor here...
+    this.pan = new Animated.ValueXY();
+
+    this.cardPanResponder = cardPanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        { dx: this.pan.x, dy: this.pan.y },
+      ]),
+      onPanResponderRelease: (e, gesture) => console.log('Released', gesture.moveY),
+    });
+  }
+
   render() {
+    const animatedStyle = {
+      transform: [
+        { translateX: this.pan.x },
+        { translateY: this.pan.y },
+      ],
+    };
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Animated.View style={[styles.card, animatedStyle]} {...this.cardPanResponder.panHandlers}>
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: 'https://graph.facebook.com/820165001428876/picture?height=500' }}
+        />
+
+        <View style={{ margin: 20 }}>
+          <Text style={{ fontSize: 20 }}>Candice, 28</Text>
+          <Text style={{ fontSize: 15, color: 'darkgrey' }}>Supermodel</Text>
+        </View>
+      </Animated.View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  card: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    margin: 10,
+    marginBottom: 100,
+    marginTop: 100,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 8,
+    overflow: 'hidden',
   },
 });
