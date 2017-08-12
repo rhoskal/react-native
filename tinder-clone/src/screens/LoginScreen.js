497d4a1567382a6fdb3e6ac01f8f5ea3ef36dc68
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-08-12 14:53:49 -0400

    facebook login done
    facebook login done

--- tinder-clone/src/screens/LoginScreen.js
+++ tinder-clone/src/screens/LoginScreen.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import Expo from 'expo';
+import firebase from 'firebase';
 
 import Facebook from '../components/login/Facebook';
 import { Colors } from '../themes';
@@ -9,10 +11,51 @@ export default class LoginScreen extends Component {
     header: null
   }
 
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.navigation.navigate('Home');
+      }
+    });
+  }
+
+  _authenticate(token) {
+    const provider = firebase.auth.FacebookAuthProvider;
+    const credential = provider.credential(token);
+
+    return firebase.auth().signInWithCredential(credential);
+  }
+
+  _createUser = (uid, userData) => {
+    firebase.database().ref('users').child(uid).update(userData);
+  }
+
+  _logIn = async () => {
+    const APP_ID = '1298495360262445';
+    const OPTIONS = {
+      permissions: [
+        'public_profile',
+        'user_birthday',
+        'user_work_history',
+        'email'
+      ]
+    };
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(APP_ID, OPTIONS);
+
+    if (type === 'success') {
+      const FIELDS = ['id', 'first_name', 'last_name', 'gender', 'birthday', 'work'];
+      const response = await fetch(`https://graph.facebook.com/me?fields=${FIELDS.toString()}&access_token=${token}`);
+      const userData = await response.json();
+      const { uid } = await this._authenticate(token);
+
+      this._createUser(uid, userData);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Facebook onPress={() => this.props.navigation.navigate('Home')} />
+        <Facebook onPress={this._logIn.bind(this)} />
       </View>
     );
   }
