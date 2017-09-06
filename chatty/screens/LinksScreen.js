07d9e00b7f7ac0cdfe4d34bf01b2041e0d64d075
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-09-06 08:36:08 -0400

    Update LinksScreen.js

--- chatty/screens/LinksScreen.js
+++ chatty/screens/LinksScreen.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
-
+class LinksScreen extends React.Component {
   render() {
     return (
-      <ScrollView style={styles.container}>
-        {/* Go ahead and delete ExpoLinksView and replace it with your
-           * content, we just wanted to provide you with some helpful links */}
-        <ExpoLinksView />
-      </ScrollView>
+      <View style={styles.container}>
+        <Text>Links</Text>
+      </View>
     );
   }
 }
+
+LinksScreen.navigationOptions = {
+  title: 'Links',
+};
+
+export default LinksScreen;
 
 const styles = StyleSheet.create({
   container: {
