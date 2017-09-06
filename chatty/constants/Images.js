b8037c8bcfbd6f35139f63b1092b086d845e9246
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-09-06 08:54:10 -0400

    Create Images.js

---
+++ chatty/constants/Images.js
@@ -0,0 +1,7 @@
+const Images = {
+  dev: require('../assets/images/robot-dev.png'),
+  prod: require('../assets/images/robot-prod.png'),
+  logo: require('../assets/images/robot-prod.png'),
+};
+
+export default Images;
