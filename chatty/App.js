2fbc04aeb5b4858c73de5c7a113b8584a34e12b9
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-09-06 08:41:22 -0400

    Update App.js

--- chatty/App.js
+++ chatty/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
+
 import RootNavigation from './navigation/RootNavigation';
 
 export default class App extends React.Component {
@@ -20,8 +21,7 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          {Platform.OS === 'android' &&
-            <View style={styles.statusBarUnderlay} />}
+          {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
           <RootNavigation />
         </View>
       );
@@ -36,16 +36,11 @@ export default class App extends React.Component {
           require('./assets/images/robot-prod.png'),
         ]),
         Font.loadAsync([
-          // This is the font that we are using for our tab bar
           Ionicons.font,
-          // We include SpaceMono because we use it in HomeScreen.js. Feel free
-          // to remove this if you are not using it in your app
           { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
         ]),
       ]);
     } catch (e) {
-      // In this case, you might want to report the error to your error
-      // reporting service, for example Sentry
       console.warn(
         'There was an error caching assets (see: App.js), perhaps due to a ' +
           'network timeout, so we skipped caching. Reload the app to try again.'
