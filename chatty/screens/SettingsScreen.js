2a67768c185618186646957bf058386407e56e31
Hans Hoffman <hansjhoffman@users.noreply.github.com>
2017-09-06 08:33:48 -0400

    Update SettingsScreen.js

--- chatty/screens/SettingsScreen.js
+++ chatty/screens/SettingsScreen.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { Text, View } from 'react-native';
 
-export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    title: 'app.json',
-  };
-
+class SettingsScreen extends React.Component {
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return (
+      <View>
+        <Text>Settings</Text>
+      </View>
+    );
   }
 }
+
+SettingsScreen.navigationOptions = {
+  title: 'app.json',
+}
+
+export default SettingsScreen;
